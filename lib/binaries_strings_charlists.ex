de7648a800a61b5afb96a3b915c02ca03839c0b0
luminecs <wangxuke.china@gmail.com>
2025-01-23 08:42:59 +0800

    fix: binaries_strings_charlists.ex

--- lib/binaries_strings_charlists.ex
+++ lib/binaries_strings_charlists.ex
@@ -7,4 +7,32 @@ defmodule BinariesStringsCharlists do
     string = "hello"
     is_binary(string)
   end
+
+  def code_point1 do
+    ?a
+  end
+
+  def code_point2 do
+    ?王
+  end
+
+  def unicode1 do
+    "\u0061" == "a"
+  end
+
+  def unicode2 do
+    0x0061 = 97 = ?a
+  end
+
+  def unicode3 do
+    String.length("hello王")
+  end
+
+  def unicode4 do
+    byte_size("hello王")
+  end
+
+  def unicode5 do
+    String.codepoints("👩‍🚒")
+  end
 end
--- test/binaries_strings_charlists_test.exs
+++ test/binaries_strings_charlists_test.exs
@@ -5,4 +5,33 @@ defmodule BinariesStringsCharlistsTest do
   test "is_binary1" do
     assert BinariesStringsCharlists.is_binary1() == true
   end
+
+  test "code_point1" do
+    assert BinariesStringsCharlists.code_point1() == 97
+  end
+
+  test "code_point2" do
+    assert BinariesStringsCharlists.code_point2() == 29579
+  end
+
+  test "unicode1" do
+    assert BinariesStringsCharlists.unicode1() == true
+  end
+
+  test "unicode2" do
+    assert BinariesStringsCharlists.unicode2() == 97
+  end
+
+  test "unicode3" do
+    assert BinariesStringsCharlists.unicode3() == 6
+  end
+
+  test "unicode4" do
+    assert BinariesStringsCharlists.unicode4() == 8
+  end
+
+  test "unicode5" do
+    assert BinariesStringsCharlists.unicode5() == ["👩", "‍", "🚒"]
+    assert BinariesStringsCharlists.unicode5() == ["👩", "\u200D", "🚒"]
+  end
 end
