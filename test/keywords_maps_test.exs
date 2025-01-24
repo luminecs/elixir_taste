5f099b1dec41bf5ea30eea774eeac6723f078069
luminecs <wangxuke.china@gmail.com>
2025-01-24 08:50:36 +0800

    fix: keywords_maps.ex

--- test/keywords_maps_test.exs
+++ test/keywords_maps_test.exs
@@ -1,5 +1,12 @@
 defmodule KeywordsMapsTest do
   use ExUnit.Case
-  doctest KeywordsMapsTest
+  doctest KeywordsMaps
 
+  test "keyword list1" do
+    assert String.split("1 2 3", " ") == ["1", "2", "3"]
+    assert String.split("1  2  3", " ") == ["1", "", "2", "", "3"]
+    # [trim: true]
+    assert String.split("1  2  3", " ", trim: true) == ["1", "2", "3"]
+    assert String.split("1  2  3", " ", trim: true, parts: 2) == ["1", " 2  3"]
+  end
 end
