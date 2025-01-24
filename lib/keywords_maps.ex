d882558bcee1b96ce22ab4d1f73b9d48bdfa51ac
luminecs <wangxuke.china@gmail.com>
2025-01-24 09:44:34 +0800

    fix: keywords_maps.ex

--- lib/keywords_maps.ex
+++ lib/keywords_maps.ex
@@ -2,4 +2,9 @@ defmodule KeywordsMaps do
   @moduledoc """
   Keyword list and maps
   """
+  
+  def key_import_function do
+    import String, only: [split: 1, split: 2]
+    split("hello world")
+  end
 end
--- test/keywords_maps_test.exs
+++ test/keywords_maps_test.exs
@@ -9,4 +9,20 @@ defmodule KeywordsMapsTest do
     assert String.split("1  2  3", " ", trim: true) == ["1", "2", "3"]
     assert String.split("1  2  3", " ", trim: true, parts: 2) == ["1", " 2  3"]
   end
+
+  test "keyword list2" do
+    assert [{:trim, true}, {:parts, 2}] == [trim: true, parts: 2]
+  end
+
+  test "key_import_function" do
+    assert KeywordsMaps.key_import_function() == ["hello", "world"]
+  end
+
+  test "keyword list4" do
+    list = [a: 1, b: 2]
+    assert list ++ [c: 3] == [a: 1, b: 2, c: 3]
+    assert [a: 0] ++ list = [a: 0, a: 1, b: 2]
+    assert list[:a] == 1
+    assert list[:b] == 2
+  end
 end
